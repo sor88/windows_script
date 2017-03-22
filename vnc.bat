97c81250ced95ce107d8c01957249b48f65490d7
plus <plus@A618.ADM>
2017-03-22 10:40:35 +0500

    Обновление скрипта с установкой с сетевой папки, без копирования в каталог temp

--- vnc.bat
+++ vnc.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
 @ECHO ON
 set dir=%\\192.168.41.100\it\
-@cls
 @ECHO ON
 
 @If exist "%programfiles%/TightVNC" (
@@ -11,17 +10,12 @@ set dir=%\\192.168.41.100\it\
 
 @If exist "%programfiles(x86)%" (
 @echo Installing: ThinVNC x 64
-
-rem @"%dir%tightvnc-2.8.5-gpl-setup-64bit.msi" /quiet
-@xcopy "%dir%*.msi*" "%temp%/*.msi" /q /y /c
-@"%temp%/tightvnc-2.8.5-gpl-setup-64bit.msi" /quiet
+@"%dir%tightvnc-2.8.5-gpl-setup-64bit.msi" /quiet
 ) else (
 
 @echo Installing: ThinVNC x 32
-@"%temp%/tightvnc-2.8.5-gpl-setup-32bit.msi" /quiet
+@"%dir%tightvnc-2.8.5-gpl-setup-32bit.msi" /quiet
 )
-del "%temp%\*.msi" /F /Q
-
 reg.exe import "%dir%vnc7-8.reg"
 @echo Done!
 )
