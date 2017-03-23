3bc9f857407f796a07895143991cd0c3e7cefe13
plus <plus@A618.ADM>
2017-03-23 16:47:03 +0500

    изменение алгоритма поиска версии ОС.

--- getver_landhcp.bat
+++ getver_landhcp.bat
@@ -1,19 +1,7 @@
 echo off 
 cls 
-for /f "tokens=2*" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') do set ProductName=%%b 
 
-if "%ProductName%" == "Windows Server 2008 R2 Datacenter" goto noconfig
-if "%ProductName%" == "Windows Server 2008 R2 Enterprise" goto noconfig
-if "%ProductName%" == "Windows Server 2008 R2 Standard" goto noconfig
-if "%ProductName%" == "Microsoft Windows Server 2003" goto noconfig
-if "%ProductName%" == "Microsoft Windows XP" goto config 
-if "%ProductName%" == "Windows 7 Ultimate" goto config
-if "%ProductName%" == "Windows 7 Enterprise" goto config
-if "%ProductName%" == "Windows 7 Professional" goto config
-if "%ProductName%" == "Windows 7 Home Premium" goto config
-if "%ProductName%" == "Windows 7 Home Basic" goto config
-if "%ProductName%" == "Windows 7 Starter" goto config
-
+reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName  | find "ProductName" | findstr /i server && call :noconfig || call :config
 
 :noconfig
 echo %ProductName%
